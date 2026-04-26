4ce3139cbdf9942c081148ac2bd278ab27910451
Svetozar Stoev <zarkoto23@gmail.com>
2026-04-26 20:14:13 +0300

    add hide edit delete buttons on details page for non logged users

--- laptop-store/src/app/components/shared/details-cart/details-cart.ts
+++ laptop-store/src/app/components/shared/details-cart/details-cart.ts
@@ -1,10 +1,11 @@
 // components/shared/details-cart/details-cart.ts
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { Observable, switchMap, map } from 'rxjs';
 import { Laptop } from '../../../models/laptop.model';
 import { LaptopService } from '../../../services/laptop.service';
 import { AsyncPipe } from '@angular/common';
+import { SupabaseService } from '../../../services/supabase.service';
 
 @Component({
   selector: 'app-details-cart',
@@ -15,17 +16,21 @@ import { AsyncPipe } from '@angular/common';
 })
 export class DetailsCart {
   laptop$: Observable<Laptop | null>;
+  currentUserId: string | null = null;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private laptopService: LaptopService
+    private laptopService: LaptopService,
+    private supabaseService: SupabaseService
   ) {
+    this.currentUserId = this.supabaseService.getCurrentUserValue()?.id || null;
+    
     this.laptop$ = this.route.paramMap.pipe(
       switchMap(params => {
         const id = params.get('id');
         return this.laptopService.getOne(id!).pipe(
-          switchMap(response => [response.data])
+          map(response => response.data)
         );
       })
     );
